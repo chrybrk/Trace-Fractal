ea0c6162442d5dfaab7b4abe3a440647f88c8ec7
NovaEmiya <importz750@gmail.com>
2021-09-02 20:27:16 +0530

    Adds stuff

--- trace8/cpu.py
+++ trace8/cpu.py
@@ -1,33 +1,90 @@
-from .conf import FONTS
+import logging
 
-__all__ = ("CPU", )
+from collections import deque
+
+from .utils import FONTS, Logger
+
+__all__ = ("CPU",)
 
 
 class CPU:
 
-    __slots__ = ("log", "memory", "display_buffer")
+    __slots__ = (
+        "_log",
+        "_memory",
+        "_display_buffer",
+        "V",
+        "pc",
+        "I",
+        "st",
+        "dt",
+        "stack",
+        "opcode",
+    )
 
-    def __init__(self, *, log=True):
-        self.log = log
+    def __init__(self, *, log: bool = True) -> None:
 
+        self.arm_registers()
         self.arm_memory_units()
         self.load_fonts()
 
-    def arm_memory_units(self):
+        self.opcode: int = 0
+
+        self._log = Logger(__name__)
+
+        if log:
+            self.debugworks()
+
+    def arm_memory_units(self) -> None:
         """
         Initializes memory and Regen Buffer
         """
         # 4 KBs of RAM as per standard limit for CHIP8 module.
         # Using 0 to represent an empty mem loc.
-        self.memory = [0] * 4096
+        self._memory = [0] * 4096
 
         # Display Buffer/Memory as per standard limit for CHIP8.
         # For storing the pixel state on the display.
-        self.display_buffer = [0] * 64 * 32
+        self._display_buffer = [0] * 64 * 32
 
-    def load_fonts(self):
+    def arm_registers(self):
+        """
+        Initializes registers.
+        """
+        self.V = [0] * 16  # 16 general purpose registers.
+        self.pc = 0x200  # program counter starting at 0x200'th (or 512'th) mem loc.
+
+        # Index counter for storing memory addresses.
+        self.I = 0  # noqa E741
+
+        # CPU interupts.
+        self.st = 0
+        self.dt = 0
+
+        # A stack for storing program counter states.
+        self.stack: deque = deque()
+
+    def load_fonts(self) -> None:
         """
         Loads fonts in the memory.
         """
         for i, item in enumerate(FONTS):
-            self.memory[i] = item
+            self._memory[i] = item
+
+    def load_rom(self, path: str) -> None:
+        """
+        loads ROM in memory.
+        """
+        logging.debug("hi")
+        with open(path, "rb") as rom:
+            for i, bnry in enumerate(rom.read()):
+                self._memory[
+                    i + 0x200
+                ] = bnry  # loading ROM in locations after fontset.
+
+    def debugworks(self) -> None:
+        """
+        Writes debug log.
+        """
+        lgr = self._log.create_logger()
+        lgr.debug(self._log.generate_log(self))
---
+++ trace8/utils.py
@@ -0,0 +1,59 @@
+import inspect
+import logging
+
+__all__ = ("FONTS", "Logger")
+
+FONTS = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0,  # 0
+    0x20, 0x60, 0x20, 0x20, 0x70,  # 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0,  # 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,  # 3
+    0x90, 0x90, 0xF0, 0x10, 0x10,  # 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,  # 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,  # 6
+    0xF0, 0x10, 0x20, 0x40, 0x40,  # 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,  # 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,  # 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90,  # A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,  # B
+    0xF0, 0x80, 0x80, 0x80, 0xF0,  # C
+    0xE0, 0x90, 0x90, 0x90, 0xE0,  # D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0,  # E
+    0xF0, 0x80, 0xF0, 0x80, 0x80,
+]  # F
+
+class Logger:
+
+    __slots__ = ("logger")
+
+    def __init__(self, name: str) ->  None:
+        self.logger = logging.getLogger(name)
+
+    def create_logger(self):
+        """
+        Creates a logger instance.
+        """
+        handler = logging.StreamHandler()
+
+        formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
+        return self.logger
+
+    @staticmethod
+    def generate_log(obj) -> str:
+        """
+        Forms a log for obj.
+        """
+        attrs = [
+            attr
+            for attr in inspect.getmembers(obj)
+            if not inspect.ismethod(attr[1])
+            if not attr[0].startswith("_")
+        ]
+        fmt = "\n".join(f"{attr}={repr(value)}" for attr, value in attrs)
+        return fmt
