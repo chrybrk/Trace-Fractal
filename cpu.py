0adf9af062df41a066d1b82a4f2ceeea84453662
Emiya97 <importz750@gmail.com>
2022-03-15 18:14:08 +0530

    Rewrites everything

---
+++ cpu.py
@@ -0,0 +1,412 @@
+from __future__ import annotations
+
+import random
+import os
+import time
+import typing as t
+
+import pygame
+
+from devices import FONTS, Display, KeyPad
+pygame.init()
+
+if t.TYPE_CHECKING:
+    from io import FileIO
+
+
+class CPU:
+    def __init__(self, speed=5, window_size=(32, 64)) -> None:
+        self.display = Display(*window_size)
+        self.keypad = KeyPad()
+
+        self.memory = bytearray(4096)
+        self.stack = [0] * 16
+
+        self.pc = 0x200
+        self.gpio = [0] * 16
+        self.stack_pointer = 0
+        self.I = 0
+        self.st = 0
+        self.dt = 0
+
+        self.opcode = 0
+
+        self.now = time.time()
+        self.speed = speed
+        self.FPS = 20
+
+        self.halt = False
+        self.draw_flag = False
+
+    @property
+    def nnn(self) -> int:
+        return self.opcode & 0xFFF
+
+    @property
+    def nn(self) -> int:
+        return self.opcode & 0x000F
+
+    @property
+    def kk(self) -> int:
+        return self.opcode & 0x00FF
+
+    @property
+    def Vx(self) -> int:
+        return (self.opcode & 0x0F00) >> 8
+
+    @property
+    def Vy(self) -> int:
+        return (self.opcode & 0x00F0) >> 4
+
+    def SYS_addr(self) -> None:
+        match self.opcode:
+            case 0x0:
+                pass
+            case 0xE0:
+                self.CLS()
+            case _:
+                decode = self.opcode & 0xF00F
+
+                try:
+                    self.functions[decode]()
+                except KeyError:
+                    self.pc += 2
+
+    def CLS(self) -> None:
+        self.draw_flag = True
+        self.display.clear()
+        self.pc += 2
+
+    def RET(self) -> None:
+        self.pc = self.stack[self.stack_pointer] + 2
+        self.stack_pointer -= 1
+
+    def JP_addr(self) -> None:
+        self.pc = self.opcode & 0xFFF
+
+    def CALL_addr(self) -> None:
+        self.stack_pointer += 1
+        self.stack[self.stack_pointer] = self.pc
+        self.pc = self.nnn
+
+    def SE_Vx_byte(self) -> None:
+        if self.gpio[self.Vx] == self.kk:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def SNE_Vx_byte(self) -> None:
+        if self.gpio[self.Vx] != self.kk:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def SE_Vx_Vy(self) -> None:
+        if self.gpio[self.Vx] == self.gpio[self.Vy]:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def LD_Vx_byte(self) -> None:
+        self.gpio[self.Vx] = self.kk
+        self.pc += 2
+
+    def ADD_Vx_byte(self) -> None:
+        self.gpio[self.Vx] += self.kk
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def CALL_8(self) -> None:
+        match self.nn:
+            case 0x0:
+                self.LD_Vx_Vy()
+            case _:
+                decode = self.opcode & 0xF00F
+                try:
+                    self.functions[decode]()
+                except KeyError:
+                    self.pc += 2
+
+    def LD_Vx_Vy(self) -> None:
+        self.gpio[self.Vx] = self.gpio[self.Vy]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def OR_Vx_Vy(self) -> None:
+        self.gpio[self.Vx] |= self.gpio[self.Vy]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def AND_Vx_Vy(self) -> None:
+        self.gpio[self.Vx] &= self.gpio[self.Vy]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def XOR_Vx_Vy(self) -> None:
+        self.gpio[self.Vx] ^= self.gpio[self.Vy]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def ADD_Vx_Vy(self) -> None:
+        value = self.gpio[self.Vx] + self.gpio[self.Vy]
+
+        if value > 0xFF:
+            self.gpio[0xF] = 1
+        else:
+            self.gpio[0xF] = 0
+
+        self.gpio[self.Vx] = value
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def SUB_Vx_Vy(self) -> None:
+        if self.gpio[self.Vx] < self.gpio[self.Vy]:
+            self.gpio[0xF] = 0
+        else:
+            self.gpio[0xF] = 1
+
+        self.gpio[self.Vx] -= self.gpio[self.Vy]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def SHR_Vx_Vy(self) -> None:
+        self.gpio[0xF] = self.gpio[self.Vx] & 0x1
+        self.gpio[self.Vx] = self.gpio[self.Vx] >> 1
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def SUBN_Vx_Vy(self) -> None:
+        if self.gpio[self.Vx] > self.gpio[self.Vy]:
+            self.gpio[0xF] = 0
+        else:
+            self.gpio[0xF] = 1
+
+        self.gpio[self.Vx] = self.gpio[self.Vy] - self.gpio[self.Vx]
+        self.gpio[self.Vx] &= 0xFF
+        self.pc += 2
+
+    def SHL_Vx_Vy(self) -> None:
+        self.gpio[0xF] = self.gpio[self.Vx] >> 7
+        self.gpio[self.Vx] = self.gpio[self.Vx] << 1
+        self.pc += 2
+
+    def SNE_Vx_Vy(self) -> None:
+        if self.gpio[self.Vx] != self.gpio[self.Vy]:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def LD_I_addr(self) -> None:
+        self.I = self.nnn
+        self.pc += 2
+
+    def JP_V0_addr(self) -> None:
+        self.pc = self.nnn + self.gpio[0x0]
+
+    def RND_Vx_byte(self) -> None:
+        self.gpio[self.Vx] = random.randint(0, 255) & self.kk
+        self.pc += 2
+
+    def DRW_Vx_Vy_nibble(self) -> None:
+        width = 8
+        height = self.opcode & 0xF
+        self.gpio[0xF] = 0
+
+        for r in range(height):
+            px = self.memory[self.I + r]
+
+            for c in range(width):
+                if (px & 0x80) > 0:
+                    erased = self.display.draw(
+                        self.gpio[self.Vx] + c, self.gpio[self.Vy] + r
+                    )
+                    if erased:
+                        self.gpio[0xF] = 1
+                px <<= 1
+
+        self.draw_flag = True
+        self.pc += 2
+
+    def CALL_E(self) -> None:
+        try:
+            self.functions[self.opcode & 0xF00F]()
+        except KeyError:
+            self.pc += 2
+
+    def SKP_Vx(self) -> None:
+        if self.keypad.keys[self.gpio[self.Vx] & 0xF] != 0:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def SKNP_Vx(self) -> None:
+        if self.keypad.keys[self.gpio[self.Vx] & 0xF] == 0:
+            self.pc += 4
+        else:
+            self.pc += 2
+
+    def CALL_F(self) -> None:
+        try:
+            self.functions[self.opcode & 0xF0FF]()
+        except KeyError:
+            self.pc += 2
+
+    def LD_Vx_DT(self) -> None:
+        self.gpio[self.Vx] = self.dt
+        self.pc += 2
+
+    def LD_Vx_K(self) -> None:
+        self.halt = True
+
+        while self.halt:
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN:
+                if event.key in self.keypad.keymap:
+                    self.keypad.keys[self.keypad.keymap[event.key]] = 1
+                    self.gpio[self.Vx] = self.keypad.keymap[event.key]
+
+                    self.halt = False
+
+        self.pc += 2
+
+    def LD_DT_Vx(self) -> None:
+        self.dt = self.gpio[self.Vx]
+        self.pc += 2
+
+    def LD_ST_Vx(self) -> None:
+        self.st = self.gpio[self.Vx]
+        self.pc += 2
+
+    def ADD_I_Vx(self) -> None:
+        self.I += self.gpio[self.Vx]
+        self.pc += 2
+
+    def LD_F_Vx(self) -> None:
+        self.I = (self.gpio[self.Vx] * 5) & 0x0FFF
+        self.pc += 2
+
+    def LD_B_Vx(self) -> None:
+        self.memory[self.I] = self.gpio[self.Vx] // 100
+        self.memory[self.I + 1] = (self.gpio[self.Vx] % 100) // 10
+        self.memory[self.I + 2] = self.gpio[self.Vx] % 10
+
+        self.pc += 2
+
+    def LD_I_Vx(self) -> None:
+        for i in range(self.Vx + 1):
+            self.memory[self.I + i] = self.gpio[i]
+        self.pc += 2
+
+    def LD_Vx_I(self) -> None:
+        for i in range(self.Vx + 1):
+            self.gpio[i] = self.memory[self.I + i]
+        self.pc += 2
+
+    @property
+    def functions(self) -> t.Callable[..., None]:
+        return {
+            0x0000: self.SYS_addr,
+            0x00E0: self.CLS,
+            0x000E: self.RET,
+            0x1000: self.JP_addr,
+            0x2000: self.CALL_addr,
+            0x3000: self.SE_Vx_byte,
+            0x4000: self.SNE_Vx_byte,
+            0x5000: self.SE_Vx_Vy,
+            0x6000: self.LD_Vx_byte,
+            0x7000: self.ADD_Vx_byte,
+            0x8000: self.CALL_8,
+            0x8001: self.LD_Vx_Vy,
+            0x8002: self.AND_Vx_Vy,
+            0x8003: self.XOR_Vx_Vy,
+            0x8004: self.ADD_Vx_Vy,
+            0x8005: self.SUB_Vx_Vy,
+            0x8006: self.SHR_Vx_Vy,
+            0x8007: self.SUBN_Vx_Vy,
+            0x800E: self.SHL_Vx_Vy,
+            0x9000: self.SNE_Vx_Vy,
+            0xA000: self.LD_I_addr,
+            0xB000: self.JP_V0_addr,
+            0xC000: self.RND_Vx_byte,
+            0xD000: self.DRW_Vx_Vy_nibble,
+            0xE000: self.CALL_E,
+            0xE00E: self.SKP_Vx,
+            0xE001: self.SKNP_Vx,
+            0xF000: self.CALL_F,
+            0xF007: self.LD_Vx_DT,
+            0xF00A: self.LD_Vx_K,
+            0xF015: self.LD_DT_Vx,
+            0xF018: self.LD_ST_Vx,
+            0xF01E: self.ADD_I_Vx,
+            0xF029: self.LD_F_Vx,
+            0xF033: self.LD_B_Vx,
+            0xF055: self.LD_I_Vx,
+            0xF065: self.LD_Vx_I,
+        }
+
+    def load_fonts(self) -> None:
+        for loc, sprite in enumerate(FONTS):
+            self.memory[loc] = sprite
+
+    def load_rom(self, file: FileIO) -> None:
+        for i, data in enumerate(file.read()):
+            self.memory[0x200 + i] = data
+
+    def beep(self) -> None:
+        os.system(
+            "play -q -n synth .1 sin 1000"
+        )
+
+    def cycle(self) -> None:
+        for _ in range(self.speed):
+            self.opcode = (self.memory[self.pc] << 8) | (self.memory[self.pc + 1])
+            decode = self.opcode & 0xF000
+
+            try:
+                self.functions[decode]()
+            except KeyError:
+                self.pc += 2
+
+        if not self.halt:
+            if self.dt > 0:
+                self.dt -= 1
+
+            if self.st > 0:
+                self.st -= 1
+                #self.beep()
+
+    def run(self) -> None:
+        self.load_fonts()
+        is_running = True
+
+        while is_running:
+            print("EXECUTING: ", hex(self.opcode & 0xF000))
+            print("PC: ", hex(self.pc))
+            print("I: ", hex(self.I))
+            print("gpio: ", self.gpio)
+            print("KEY", self.keypad.keys)
+            print("----------------------------------------")
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key in self.keypad.keymap:
+                        self.keypad.keys[self.keypad.keymap[event.key]] = 1
+
+                if event.type == pygame.KEYUP:
+                    if event.key in self.keypad.keymap:
+                        self.keypad.keys[self.keypad.keymap[event.key]] = 0
+
+                if event.type == pygame.QUIT:
+                    is_running = False
+
+            if (time.time() - self.now) > (self.FPS/1000):
+                self.cycle()
+
+            if self.draw_flag:
+                self.display.render()
+
+
+if __name__ == "__main__":
+    cpu = CPU(speed=1)
+    cpu.load_rom(open("./roms/test_opcode.ch8", "rb"))
+    cpu.run()
---
+++ devices.py
@@ -0,0 +1,99 @@
+import pygame
+
+__all__ = ( "FONTS", "Display", "KeyPad")
+
+
+_0 = (0, 0, 0)
+_1 = (0,255,255)
+
+# fmt: off
+FONTS = [ 
+        0xF0, 0x90, 0x90, 0x90, 0xF0, # 0
+        0x20, 0x60, 0x20, 0x20, 0x70, # 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, # 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, # 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, # 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, # 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, # 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, # 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, # 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, # 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, # A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, # B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, # C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, # D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, # E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  # F
+        ]
+# fmt: on
+
+
+class Display:
+    def __init__(self, rows: int, columns: int) -> None:
+        self.rows = rows
+        self.columns = columns
+
+        self.extendby = 10
+        self.screen = pygame.display.set_mode(
+            (columns * self.extendby, rows * self.extendby)
+        )
+        self.screen.fill(_0)
+        pygame.display.flip()
+
+        self.display_buffer = bytearray(rows * columns)
+
+    def draw(self, xpos: int, ypos: int) -> bool:
+        if 0 > xpos:
+            xpos += self.columns
+        xpos -= self.columns if xpos > self.columns else 0
+
+        if 0 > ypos:
+            ypos += self.rows
+        ypos -= self.rows if ypos > self.rows else 0
+
+        loc = xpos + (ypos * self.columns)
+        try:
+            self.display_buffer[loc] ^= 1
+            return not self.display_buffer[loc]
+        except IndexError:
+            return 0
+
+    def clear(self) -> None:
+        self.display_buffer = [0] * self.rows * self.columns
+
+    def render(self) -> None:
+        self.screen.fill(_0)
+
+        for i in range(self.columns * self.rows):
+
+            x = (i % self.columns) * self.extendby
+            y = (i // self.columns) * self.extendby
+
+            if self.display_buffer[i]:
+                pygame.draw.rect(
+                    self.screen, _1, pygame.Rect(x, y, self.extendby, self.extendby)
+                )
+        pygame.display.update()
+
+
+class KeyPad:
+    def __init__(self) -> None:
+        self.keys = [0] * 16
+        self.keymap = {
+            pygame.K_1: 1,
+            pygame.K_2: 2,
+            pygame.K_3: 3,
+            pygame.K_4: 12,
+            pygame.K_q: 4,
+            pygame.K_w: 5,
+            pygame.K_e: 6,
+            pygame.K_r: 13,
+            pygame.K_a: 7,
+            pygame.K_s: 8,
+            pygame.K_d: 9,
+            pygame.K_f: 14,
+            pygame.K_z: 10,
+            pygame.K_x: 0,
+            pygame.K_b: 11,
+            pygame.K_v: 15,
+        }
